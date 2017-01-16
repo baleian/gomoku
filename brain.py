b7429d164c41450dbaf3d2f8dd8aece94b52b7d1
baleian <baleian90@gmail.com>
2017-01-17 04:33:53 +0900

    test apply minimax

--- brain.py
+++ brain.py
@@ -4,29 +4,182 @@ class Brain:
   def __init__(self):
     pass
 
-  def predict(self, board, stone, rule):
-    data = board.getData()
-    size = board.getSize()
+  def _predict(self, board, stone, rule, depth):
     opponent = 2 if stone is 1 else 1
+    size = board.getSize()
 
+    tr, tc = -1, -1
+    moreThink = False
     possibles = []
-    ret = None
+    list10 = []
 
     for r in range(size):
       for c in range(size):
         valid = rule.verify(board, r, c, stone)
-        
+        valid2 = rule.verify(board, r, c, opponent)
+
         if valid.isForbidden:
           continue
-
-        if valid.isWin:
-          ret = (r, c)
-          break
-
-        opvalid = rule.verify(board, r, c, opponent)
-        if opvalid.isWin:
-          ret = (r, c)
-
+        
         possibles.append((r, c))
 
-    return ret if ret is not None else random.choice(possibles)
+        if valid.isWin:
+          if depth is 2:
+            pipeWrite('MESSAGE valid iswin!(%d, %d, %d) = %d' % (r, c, 10, depth))
+          return r, c, 10
+        if valid2.isWin:
+          tr, tc = r, c
+          moreThink = True
+        if valid.n4mok >= 2:
+          list10.append((r, c))
+          moreThink = True
+        if valid.n4mok > 0 or valid2.n4mok > 0:
+          moreThink = True
+
+    if tr is not -1 and tc is not -1:
+      if depth is 2:
+        pipeWrite('MESSAGE valid2 iswin!(%d, %d, %d) = %d' % (tr, tc, 5, depth))
+      return tr, tc, 5
+
+    if len(list10) > 0:
+      r, c = random.choice(list10)
+      if depth is 2:
+        pipeWrite('MESSAGE valid n4mok >= 2!(%d, %d, %d) = %d' % (r, c, 10, depth))
+      return r, c, 10
+
+    if not moreThink:
+      r, c = random.choice(possibles)
+      return r, c, 5
+
+    if depth is 1:
+      r, c = random.choice(possibles)
+      return r, c, 5
+
+    list5 = []
+    list0 = []
+    for r, c in possibles:
+      board.putStone(r, c, stone)
+      rr, cc, ww = self._predict(board, opponent, rule, depth - 1)
+      board.popStone(r, c)
+
+      ww = 10 - ww
+      if ww is 10:
+        return r, c, 10
+      elif ww is 5:
+        list5.append((r, c))
+      else:
+        list0.append((r, c))
+        
+
+    if len(list5) > 0:
+      r, c = random.choice(list5)
+      return r, c, 5
+
+    r, c = random.choice(list0)
+    return r, c, 0
+
+
+  def predict(self, board, stone, rule):
+    r, c, w = self._predict(board, stone, rule, 2)
+    if w is not 5:
+      pipeWrite('MESSAGE predict!(%d, %d, %d)' % (r, c, w))
+    return r, c
+
+
+  def _minimax(self, board, stone, rule, depth):
+    if depth is 0:
+      return 5
+    
+    opponent = 2 if stone is 1 else 1
+    size = board.getSize()
+    isMinimax = False
+    possibles = []
+
+    retMax = 0
+    for r in range(size):
+      for c in range(size):
+        valid = rule.verify(board, r, c, stone)
+        if valid.isWin:
+          return 10
+        valid2 = rule.verify(board, r, c, opponent)
+        if valid2.isWin:
+          return 10
+        if valid.n4mok > 0:
+          isMinimax = True
+        if valid2.n4mok > 0:
+          isMinimax = True
+
+        if not valid.isForbidden:
+          possibles.append((r, c))
+    
+    if isMinimax:
+      for r, c in possibles:
+        board.putStone(r, c, stone)
+        ret = 10 - self._minimax(board, opponent, rule, depth - 1)
+        board.popStone(r, c)
+        if retMax < ret:
+          retMax = ret
+
+    return retMax
+
+
+  def minimax(self, board, stone, rule, depth=2):
+    opponent = 2 if stone is 1 else 1
+    size = board.getSize()
+
+    possibles = []
+    list10 = []
+    list5 = []
+    list0 = []
+
+    isMinimax = False
+
+    for r in range(size):
+      for c in range(size):
+        valid = rule.verify(board, r, c, stone)
+        if valid.isWin:
+          return r, c
+        valid2 = rule.verify(board, r, c, opponent)
+        if valid2.isWin:
+          return r, c
+        if valid.n4mok > 0:
+          isMinimax = True
+        if valid2.n4mok > 0:
+          isMinimax = True
+
+        if not valid.isForbidden:
+          possibles.append((r, c))
+
+    if isMinimax:
+      pipeWrite("MESSAGE IsMINIMAX!!!!")
+      for r, c in possibles:
+        board.putStone(r, c, stone)
+        ret = 10 - self._minimax(board, opponent, rule, depth - 1)
+        board.popStone(r, c)
+
+        if ret is 10:
+          list10.append((r, c))
+        elif ret is 5:
+          list5.append((r, c))
+        else:
+          list0.append((r, c))
+              
+      if len(list10) > 0:
+        return random.choice(list10)
+
+      if len(list5) > 0:
+        return random.choice(list5)
+
+      if len(list0) > 0:
+        return random.choice(list0)        
+
+    return random.choice(possibles)
+
+
+
+import sys
+def pipeWrite(str):
+  sys.stdout.write(str)
+  sys.stdout.write('\n')
+  sys.stdout.flush()
+
--- rule.py
+++ rule.py
@@ -8,22 +8,25 @@ class Rule():
 
 
 class Valid():
-  def __init__(self, isWin, isForbidden):
+  def __init__(self, isWin, isForbidden, n3mok, n4mok):
     self.isWin = isWin
     self.isForbidden = isForbidden
+    self.n3mok = n3mok
+    self.n4mok = n4mok
 
 
 class Renju():
   def verify(self, board, r, c, stone):
     if not board.putStone(r, c, stone):
-      return Valid(isWin=False, isForbidden=True)
+      return Valid(isWin=False, isForbidden=True, n3mok=0, n4mok=0)
     patterns = board.getPatterns(r, c)
     board.popStone(r, c)
 
-    return Valid(
-      isWin=self._isWin(patterns, stone),
-      isForbidden=self._isForbidden(patterns, stone)
-    )
+    isWin = self._isWin(patterns, stone)
+    isForbidden = False if isWin else self._isForbidden(patterns, stone)
+    n3mok = 0 if isForbidden else self._n3mok(patterns, stone)
+    n4mok = 0 if isForbidden else self._n4mok(patterns, stone)
+    return Valid(isWin, isForbidden, n3mok, n4mok)
 
 
   def _isWin(self, patterns, stone):
@@ -42,22 +45,22 @@ class Renju():
     if self._isWin(patterns, stone):
       return False
 
-    if (self._sixmok(patterns) or
-      self._check33(patterns) or
-      self._check44(patterns)):
+    if (self._sixmok(patterns, stone) or
+      self._check33(patterns, stone) or
+      self._check44(patterns, stone)):
       return True
 
     return False
 
 
-  def _sixmok(self, patterns):
+  def _sixmok(self, patterns, stone):
     for i in range(4):
       if re.search(r'111111', patterns[i]): 
         return True
     return False
 
 
-  def _check33(self, patterns):
+  def _check33(self, patterns, stone):
     cnt = 0
     for i in range(4):
       s = patterns[i]
@@ -69,20 +72,55 @@ class Renju():
     return cnt >= 2
 
 
-  def _check44(self, patterns):
+  def _check44(self, patterns, stone):
     cnt = 0
     for i in range(4):
       s = patterns[i]
-      if (re.search(r'01111[^1]', s) or 
-        re.search(r'[^1]11110', s) or
-        re.search(r'[^1]10111[^1]', s) or
-        re.search(r'[^1]11011[^1]', s) or
-        re.search(r'[^1]11101[^1]', s)):
-        cnt = cnt + 1
-      if (re.search(r'[^1]1011101[^1]', s) or 
-        re.search(r'[^1]11011011[^1]', s) or 
-        re.search(r'[^1]111010111[^1]', s)):
-        cnt = cnt + 2
+      if stone is 1:
+        if re.search(r'[^1]01111[^1]', s) or re.search(r'[^1]11110[^1]', s): cnt = cnt + 1
+        if re.search(r'[^1]10111[^1]', s): cnt = cnt + 1
+        if re.search(r'[^1]11011[^1]', s): cnt = cnt + 1
+        if re.search(r'[^1]11101[^1]', s): cnt = cnt + 1
+      elif stone is 2:
+        if re.search(r'02222', s) or re.search(r'22220', s): cnt = cnt + 1
+        if re.search(r'20222', s): cnt = cnt + 1
+        if re.search(r'22022', s): cnt = cnt + 1
+        if re.search(r'22202', s): cnt = cnt + 1
+
     return cnt >= 2
 
 
+  def _n3mok(self, patterns, stone):
+    cnt = 0
+    for i in range(4):
+      s = patterns[i]
+      if stone is 1:
+        if re.search(r'001110[^1]', s) or re.search(r'[^1]011100', s): cnt = cnt + 1
+        if re.search(r'[^1]011010[^1]', s): cnt = cnt + 1
+        if re.search(r'[^1]010110[^1]', s): cnt = cnt + 1
+      elif stone is 2:
+        if re.search(r'002220[^2]', s) or re.search(r'[^2]022200', s): cnt = cnt + 1
+        if re.search(r'020220', s): cnt = cnt + 1
+        if re.search(r'022020', s): cnt = cnt + 1
+    
+    return cnt
+
+
+  def _n4mok(self, patterns, stone):
+    cnt = 0
+    for i in range(4):
+      s = patterns[i]
+      if stone is 1:
+        if re.search(r'[^1]01111[^1]', s): cnt = cnt + 1
+        if re.search(r'[^1]11110[^1]', s): cnt = cnt + 1
+        if re.search(r'[^1]10111[^1]', s): cnt = cnt + 1
+        if re.search(r'[^1]11011[^1]', s): cnt = cnt + 1
+        if re.search(r'[^1]11101[^1]', s): cnt = cnt + 1
+      elif stone is 2:
+        if re.search(r'02222', s): cnt = cnt + 1
+        if re.search(r'22220', s): cnt = cnt + 1
+        if re.search(r'20222', s): cnt = cnt + 1
+        if re.search(r'22022', s): cnt = cnt + 1
+        if re.search(r'22202', s): cnt = cnt + 1
+    
+    return cnt
